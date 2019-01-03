dc30cc30f12ee4dfc61b9bf4c83ea99713394a6f
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2019-01-02 22:26:47 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,8 @@ var prefix = "!zg "
 
 client.on('ready',() => {
   console.log("I\'m Online! Ready to launch!");
-  client.user.setActivity('!zg help | www.zg-gaming.co', { type: 'PLAYING' })
-    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : '!zg help | www.zg-gaming.co'}`))
+  client.user.setActivity('!zg help | zg-gaming.co', { type: 'PLAYING' })
+    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : '!zg help | zg-gaming.co'}`))
     .catch(console.error);
 })
 
@@ -16,7 +16,7 @@ client.on('message', message => {
   		message.channel.send({embed: { "plainText": "", "title": "Bot Support | Zero Gravity Gaming", "description": "Commands: \n • !zg help - Shows this this message \n • !zg servers - Displays zG| Servers \n • !zg about - About zG| and bot ", "color": 53380 }});
   	}
     if (message.content.startsWith(prefix + 'servers')) {
-  		message.channel.send({embed: { "plainText": "", "title": "zG| Global Servers", "description": "\n Call of Duty 4: Modern Warfare Servers: \n • zG| Sniper Online Zero Gravity - Round: 1/20 \n :flag_us: `74.91.127.206:28961` \n • zG| ProMod 2.20 // Zero Gravity - Round: 1/20 \n :flag_us: `74.91.127.206:28960` \n • zG| ProMod 2.16 // Zero Gravity \n :flag_us: `74.91.127.206:28963` \n • zG| CoDJumper // Zero Gravity \n :flag_us: `74.91.127.206:28964` \n \n Teamspeak 3: \n :flag_us: `	74.91.127.206:9987`", "color": 53380 }});
+  		message.channel.send({embed: { "plainText": "", "title": "zG| Global Servers", "description": "\n Call of Duty 4: Modern Warfare Servers: \n • zG| Sniper Online Zero Gravity \n :flag_us: `74.91.127.206:28961` \n • zG| ProMod 2.20 // Zero Gravity \n :flag_us: `74.91.127.206:28960` \n • zG| ProMod 2.16 // Zero Gravity \n :flag_us: `74.91.127.206:28963` \n • zG| CoDJumper // Zero Gravity \n :flag_us: `74.91.127.206:28964` \n \n Teamspeak 3: \n :flag_us: `	74.91.127.206:9987`", "color": 53380 }});
   	}
     if (message.content.startsWith(prefix + 'about')) {
     message.channel.send({embed: { "plainText": "", "title": "About zG| & Discord bot", "description": "Zero Gravity Gaming was founded by zG| NeRy & zG| Thief in 2013 \n Becoming one of the most famous clans in COD4:MW history \n Currently zG|NeRy is not at power \n zG|REY305 is now the Head-Leader of zG. \n Website: https://zg-gaming.co/", "color": 53380 }});
