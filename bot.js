314e3a91f51cf6a36ab5d356236c1bb8ba7cf717
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2019-01-02 21:49:05 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-var prefix = "!zg "
+var prefix = "!zg"
 
 client.on('ready',() => {
   console.log("I\'m Online! Ready to launch!");
