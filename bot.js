cb1833665d556306d9fc3fda58ce093cd9e5abf1
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2019-01-02 22:21:27 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('message', message => {
   		message.channel.send({embed: { "plainText": "", "title": "zG| Global Servers", "description": "Call of Duty 4: Modern Warfare Servers: \n • zG| Sniper Online Zero Gravity - Round: 1/20 \n :flag_us: `74.91.127.206:28961` \n • zG| ProMod 2.20 // Zero Gravity - Round: 1/20 \n :flag_us: `74.91.127.206:28960` \n • zG| ProMod 2.16 // Zero Gravity \n :flag_us: `74.91.127.206:28963` \n • zG| CoDJumper // Zero Gravity \n :flag_us: `74.91.127.206:28964` \n \n Teamspeak 3: \n :flag_us: `	74.91.127.206:9987`", "color": 53380 }});
   	}
     if (message.content.startsWith(prefix + 'about')) {
-    message.channel.send({embed: { "plainText": "", "title": "About zG| & Discord bot", "description": "Zero Gravity Gaming was founded by zG| NeRy & zG| Thief in 2013 \n Becoming one of the most famous clans in COD4:MW history \n Currently zG|NeRy is the Main-Owner & Administrator \n Website: https://zg-gaming.co/", "color": 53380 }});
+    message.channel.send({embed: { "plainText": "", "title": "About zG| & Discord bot", "description": "Zero Gravity Gaming was founded by zG| NeRy & zG| Thief in 2013 \n Becoming one of the most famous clans in COD4:MW history \n Currently zG|NeRy is not at power \n zG|REY305 is now the Head-Leader of zG. \n Website: https://zg-gaming.co/", "color": 53380 }});
     }
 
 });
