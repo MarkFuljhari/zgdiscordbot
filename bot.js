2417f9c03908a583675d76bbf0d97dd336732672
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2022-01-17 20:52:41 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,21 +4,26 @@ const client = new Discord.Client();
 var prefix = "!zg "
 
 client.on('ready',() => {
- console.log('Robot is ready to operate!');
-  console.log(`Robot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-  client.user.setActivity('!zg help | zg-gaming.co', { type: 'WATCHING' })
+ console.log('Discord Bot is ready to operate! You can test the commands.');
+ console.log(`Discord Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guild (s).`);
+ console.log('Activation only allowed with permission of Administrators or Developers!');
+ client.user.setActivity('!zg help | zg-gaming.co', { type: 'PLAYING' })
     .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : '!zg help | zg-gaming.co'}`))
     .catch(console.error);
 })
 
 client.on('message', message => {
   if (message.author === client.user) return;
-  	if (message.content.startsWith(prefix + 'help')) {
+ 
+// COMMANDS
+   	if (message.content.startsWith(prefix + 'help')) {
   		message.channel.send({embed: { "plainText": "", "title": "Bot Support | Zero Gravity Gaming", "description": "Commands: \n • !zg help - Shows this message \n • !zg servers - Displays zG| Servers \n • !zg about - About zG| and bot ", "color": 53380 }});
-  	}
+   	}
+ 
     if (message.content.startsWith(prefix + 'servers')) {
   		message.channel.send({embed: { "plainText": "", "title": "zG| Global Servers", "description": "\n Call of Duty 4: Modern Warfare Servers: \n • zG| Sniper Online Zero Gravity \n :flag_us: `74.91.127.206:28961` \n • zG| ProMod 2.20 // Zero Gravity \n :flag_us: `74.91.127.206:28960` \n • zG| ProMod 2.16 // Zero Gravity \n :flag_us: `74.91.127.206:28963` \n • zG| CoDJumper // Zero Gravity \n :flag_us: `74.91.127.206:28964` \n \n Teamspeak 3: \n :flag_us: `	74.91.127.206:9987`", "color": 53380 }});
-  	}
+   	}
+ 
     if (message.content.startsWith(prefix + 'about')) {
     message.channel.send({embed: { "plainText": "", "title": "About zG| & Discord bot", "description": "Zero Gravity Gaming was founded by zG| NeRy & zG| Thief in 2013 \n Becoming one of the most famous clans in COD4:MW history \n Currently zG|NeRy is not at power \n zG|REY305 is now the Head-Leader of zG. \n Website: https://zg-gaming.co/", "color": 53380 }});
     }
